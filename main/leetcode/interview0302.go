c9a6222558fe53d071ddf07a6e576f3136d0a2eb
michealchen <michealchen730@gmail.com>
2020-06-19 09:55:11 +0800

    got some new problems solved

--- main/leetcode/interview0302.go
+++ main/leetcode/interview0302.go
@@ -1,27 +1,27 @@
 package main
 
 //应该是考察单调栈
-type MinStack struct {
+type MinStack0302 struct {
 	stack []int
 	mins  []int
 }
 
 /** initialize your data structure here. */
-func Constructor() MinStack {
-	return MinStack{
+func Constructor0302() MinStack0302 {
+	return MinStack0302{
 		stack: []int{},
 		mins:  []int{}, //存的是元素的数组下标
 	}
 }
 
-func (this *MinStack) Push(x int) {
+func (this *MinStack0302) Push(x int) {
 	this.stack = append(this.stack, x)
 	if len(this.mins) == 0 || this.stack[this.mins[len(this.mins)-1]] > x {
 		this.mins = append(this.mins, len(this.stack)-1)
 	}
 }
 
-func (this *MinStack) Pop() {
+func (this *MinStack0302) Pop() {
 	if len(this.stack) != 0 {
 		this.stack = this.stack[:len(this.stack)-1]
 		for len(this.mins) > 0 && this.mins[len(this.mins)-1] >= len(this.stack) {
@@ -30,14 +30,14 @@ func (this *MinStack) Pop() {
 	}
 }
 
-func (this *MinStack) Top() int {
+func (this *MinStack0302) Top0302() int {
 	if len(this.stack) != 0 {
 		return this.stack[len(this.stack)-1]
 	}
 	return -1
 }
 
-func (this *MinStack) GetMin() int {
+func (this *MinStack0302) GetMin0302() int {
 	if len(this.mins) != 0 {
 		return this.stack[this.mins[len(this.mins)-1]]
 	}
